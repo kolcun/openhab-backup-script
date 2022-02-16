dcf03bea269352fcf89fe7a553c6cc24847591e0
Mike Kolcun <mike.kolcun@gmail.com>
2022-02-16 10:21:02 -0500

    Free space check

---
+++ free-space-check.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+push () {
+    curl -s -F "token=a87h419p6yvbak54261w8tp1aixjfr" \
+    -F "user=uskVCNTGyLQGVoETqHiZaBhG75SU2L" \
+    -F "title=Openhab Free Space" \
+    -F "message=$1" https://api.pushover.net/1/messages.json
+}
+
+freeSpaceOnDisk=`df -kh | grep  '^/dev/root' | awk '{ print $4 }' |  cut -d'G' -f1`
+echo "Space on disk: $freeSpaceOnDisk"
+if [ $freeSpaceOnDisk -le 40 ]; then
+	echo "Less than 40GB free"
+	push "WARNING: Free Space less than 40GB"
+else
+	push "OK: Free Space is $freeSpaceOnDisk GB"
+fi
