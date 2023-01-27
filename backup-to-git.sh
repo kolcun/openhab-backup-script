1499ca0a246617ec3f4eb7648473360d8d037f86
Mike Kolcun <mike.kolcun@gmail.com>
2023-01-27 15:00:06 -0500

    add pushover notification for git push fails

--- backup-to-git.sh
+++ backup-to-git.sh
@@ -5,14 +5,38 @@ commitMessage="Daily Backup: $currentDate"
 
 logger Daily openhab backup: start
 
+
+function push {
+    curl -s -F "token=a87h419p6yvbak54261w8tp1aixjfr" \
+    -F "user=uskVCNTGyLQGVoETqHiZaBhG75SU2L" \
+    -F "title=Openhab3 Backup Cron Script" \
+    -F "message=$1" https://api.pushover.net/1/messages.json
+}
+
 cd /etc/openhab
 git add .
 git commit -m "$commitMessage"
-git push
+if ! git push
+then
+        push "/etc/openhab github push failed"
+fi
+
 
 cd /var/lib/openhab
 git add .
 git commit -m "$commitMessage"
-git push
+if ! git push
+then
+        push "/var/lib/openhab github push failed"
+fi
+
+cd /home/pi/.node-red
+git add *.js*
+git add .config*.json
+git commit -m "$commitMessage"
+if ! git push
+then
+	push "Nodered github push failed"
+fi
 
 logger Daily openhab backup: complete
--- backup-zigbee2mqtt-to-git.sh
+++ backup-zigbee2mqtt-to-git.sh
@@ -5,6 +5,13 @@ commitMessage="Daily Backup: $currentDate"
 
 logger Daily zigbee2mqtt backup: start
 
+function push {
+    curl -s -F "token=a87h419p6yvbak54261w8tp1aixjfr" \
+    -F "user=uskVCNTGyLQGVoETqHiZaBhG75SU2L" \
+    -F "title=Openhab3 Backup Cron Script" \
+    -F "message=$1" https://api.pushover.net/1/messages.json
+}
+
 backupLocation="/opt/zigbee2mqtt-backup/"
 
 cp /opt/zigbee2mqtt/data/configuration.yaml $backupLocation
@@ -14,5 +21,9 @@ cp /opt/zigbee2mqtt/data/*.js $backupLocation
 cd $backupLocation
 git add .
 git commit -m "$commitMessage"
-git push
+if ! git push
+then
+        push "zigbee github push failed"
+fi
+
 
